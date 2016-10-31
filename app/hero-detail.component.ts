35189a27858ed7e95e7c172c7a50584de6415934
user <truebatmandc@gmail.com>
2016-10-31 14:35:25 +0200

    Finished the tutorial 'angular-tour-of-heroes'.

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { ActivatedRoute, Params} from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from './hero.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-hero-detail',
+  templateUrl: 'hero-detail.component.html',
+  styleUrls: [ 'hero-detail.component.css']
+})
+
+export class HeroDetailComponent implements OnInit {
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.forEach((params: Params) => {
+      let id = +params['id'];
+      this.heroService.getHero(id).then(hero => this.hero = hero);
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
+
+  @Input() hero: Hero;
+}
